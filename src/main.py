724ddc72e5c77606a68d8f2a8a4b4dee478b65ae
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2023-06-12 13:49:37 +0200

    fix: Avoid circular import

--- src/main.py
+++ src/main.py
@@ -2,13 +2,15 @@ from flask import Flask
 import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from src.models import room, user, user_room, message
+from src.extensions import db, migrate
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+db.init_app(app)
+migrate.init_app(app, db)
 
 @app.route("/")
 def index():
